f08bbe50c0780c0e57fb23ac98dcdd173012f0b8
naj317 <naj317@lehigh.edu>
2014-09-01 20:20:56 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//
+//Natalie Jacobs
+//Sept 2 2014
+//CSE2
+//HW 1 Welcome Java Program
+//
+//  compile program
+//      javac WelcomeClass.java
+//  run program
+//      java Welcome Class
+//
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main (String [] args) {
+    
+//  print welcome statement, with signature, and short autobiography
+       System.out.println("-----------");
+       System.out.println("| WELCOME |");
+       System.out.println("-----------");
+       System.out.println("  ^  ^  ^  ^  ^  ^  ");
+       System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+       System.out.println("|-N--A--J--3--1--7-|");
+       System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+       System.out.println("  v  v  v  v  v  v  ");
+       System.out.println("This summer I went to Georgia with my family where we went hiking, white-water rafting, and zipling!");
+
+//  test code
+    }
+}
